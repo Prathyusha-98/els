88fb7dbe7906a4b436e73c4be527477b89706f89
Prathyusha <prathyusha@simplemail.ai>
2023-07-21 11:36:05 +0530

    added alias in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname)
+    return config
+  },
+}
 
 module.exports = nextConfig
 
