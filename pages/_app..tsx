292ce34bdbf2b2108c7fafc4700390547d609f63
Prathyusha <prathyusha@simplemail.ai>
2023-09-08 16:36:27 +0530

    favicon

---
+++ pages/_app..tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import '../app/layout'
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Head>
+      <title>
+        Simple AI
+      </title>
+      <link rel="shortcut icon" href="/favicon" />
+    </Head>
+    <Component {...pageProps} />
+    </>
+  );
+}
